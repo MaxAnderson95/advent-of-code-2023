28b086eb665a8ceca496f7af142bb01f7e53ca95
Max Anderson <max.a.anderson95@gmail.com>
2023-12-01 18:51:10 -0500

    Updated the test and configured it to raise so you know which failed

--- 1/test.py
+++ 1/test.py
@@ -1,10 +1,12 @@
-from solution import get_input, solution_part_one
+from solution import get_input, solution_part_one, solution_part_two
 
 input = get_input()
 
 try:
     assert solution_part_one(input) == 56465
+    assert solution_part_two(input) == 55902
 except AssertionError:
     print("Tests did NOT pass")
+    raise
 else:
     print("Tests passed successfully!")
