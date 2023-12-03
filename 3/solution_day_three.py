8e89213bc3e4f98cef0da04b5e9d3f4ef32fb42c
Max Anderson <max.a.anderson95@gmail.com>
2023-12-03 15:44:04 -0500

    Added part two

--- 3/solution_day_three.py
+++ 3/solution_day_three.py
@@ -1,6 +1,7 @@
 import sys
 import pathlib
 import itertools
+from collections import defaultdict
 
 
 def get_input(file_name: str = "input.txt") -> list[str]:
@@ -51,6 +52,48 @@ def solution_part_one(input: list[str]) -> int:
     return sum(valid_numbers)
 
 
+def solution_part_two(input: list[str]) -> int:
+    valid_numbers: list[int] = []
+    # gear_ratios: dict[tuple(int, int), int] = {}
+    valid_gears = defaultdict(list[int])
+    for i, line in enumerate(input):  # Loop through each line
+        is_valid = False  # So far we've not found a valid number
+        num = ''  # Create a number builder string
+        for j, char in enumerate(line):  # Loop through each chracter on the line
+            if char.isdigit():  # If it's a digit
+                num += char  # Start building the number string
+                # Now we check the coordinates in the 8 spots around it for a gear
+                for i_offset, j_offset in offsets:
+                    try:
+                        position_to_check = input[i + i_offset][j + j_offset]
+                        if position_to_check == '*':  # If it's a gear
+                            # We've hit a gear in the 9x9 grid around the char
+                            # We mark the entire number as being valid.
+                            is_valid = True
+                            gear_location = (i + i_offset, j + j_offset)
+                    except IndexError:
+                        continue  # Ignore when we're on the edge or corner of the grid when we check out of bounds
+
+            # If we spot a non-digit or hit the end of the row, we've found all the digits in that whole number
+            if not char.isdigit() or j == len(line) - 1:
+                # If that whole number we found is valid (adjacent to a gear)
+                if is_valid:
+                    # Add it to the final list
+                    valid_gears[(gear_location)].append(int(num))
+                    is_valid = False  # Reset back to false
+                num = ''  # Reset number builder string back to empty
+
+    # Convert back to a regular dict
+    valid_gears = dict(valid_gears)
+
+    # Filter for the gears coordinates that appear exactly twice, then multiply those together
+    gear_ratios = [valid_gears[v][0] * valid_gears[v][1]
+                   for _, v in enumerate(valid_gears) if len(valid_gears[v]) == 2]
+
+    return sum(gear_ratios)
+
+
 if __name__ == "__main__":
     input = get_input()
     print(f"Solution for part 1: {solution_part_one(input)}")
+    print(f"Solution for part 2: {solution_part_two(input)}")
--- 3/test_day_three.py
+++ 3/test_day_three.py
@@ -1,4 +1,4 @@
-from solution_day_three import get_input, solution_part_one
+from solution_day_three import get_input, solution_part_one, solution_part_two
 
 input = get_input()
 
@@ -6,6 +6,10 @@ input = get_input()
 def test_solution_part_one() -> None:
     print(input)
     assert solution_part_one(input) == 539713
+
+
+def test_solution_part_two() -> None:
+    assert solution_part_two(input) == 84159075
 
 
 if __name__ == "__main__":
