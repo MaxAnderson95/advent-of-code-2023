217d2ae0db906252eb33e711bb13e76551d7c603
Max Anderson <max.a.anderson95@gmail.com>
2023-12-09 22:54:15 -0500

    Added tests

---
+++ 2023/5/test_day_five.py
@@ -0,0 +1,16 @@
+from solution_day_five import get_input, solution_part_one
+
+input = get_input()
+
+
+def test_solution_part_one() -> None:
+    assert solution_part_one(input) == 836040384
+
+
+# def test_solution_part_two() -> None:
+#     assert solution_part_two(input) == ...
+
+
+if __name__ == "__main__":
+    test_solution_part_one()
+    # test_solution_part_two()
